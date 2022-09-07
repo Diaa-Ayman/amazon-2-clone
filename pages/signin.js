133e9ea2d71850024cc9e1f7f63fb4e394277595
Diaa Ayman <abugamel53@gmail.com>
2022-09-07 20:08:08 +0200

    again

--- pages/signin.js
+++ pages/signin.js
@@ -5,7 +5,7 @@ import React from "react";
 
 function Signin() {
   return (
-    <div class="w-full m-auto mt-8 max-w-[350px]">
+    <div className="w-full m-auto mt-8 max-w-[350px]">
       <Head>
         <title>Amazon Sign in</title>
       </Head>
@@ -16,41 +16,41 @@ function Signin() {
         />
       </div>
 
-      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="mb-8">
           <span className="font-semibold text-2xl text-gray-800">Sign In</span>
         </div>
-        <div class="mb-4">
+        <div className="mb-4">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-gray-700 text-sm font-bold mb-2"
             for="username"
           >
             Username
           </label>
           <input
-            class="shadow-lg focus:shadow-yellow-600  appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow-lg focus:shadow-yellow-600  appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="username"
             type="text"
             placeholder="Username"
           />
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-gray-700 text-sm font-bold mb-2"
             for="password"
           >
             Password
           </label>
           <input
-            class="shadow-lg focus:shadow-yellow-600 appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow-lg focus:shadow-yellow-600 appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             type="password"
             placeholder="Enter Password"
           />
         </div>
-        <div class="flex items-center justify-center">
+        <div className="flex items-center justify-center">
           <button
-            className="button text-center w-full py-1 font-semibold text-lg"
+            classNameName="button text-center w-full py-1 font-semibold text-lg"
             type="button"
           >
             Sign In
