9d533ffbc3b702429ccb271117f4b7d7a9fc008d
Diaa Ayman <abugamel53@gmail.com>
2022-09-07 20:10:09 +0200

    againn

--- pages/signin.js
+++ pages/signin.js
@@ -23,7 +23,7 @@ function Signin() {
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
-            for="username"
+            htmlFor="username"
           >
             Username
           </label>
@@ -37,7 +37,7 @@ function Signin() {
         <div className="mb-6">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
-            for="password"
+            htmlFor="password"
           >
             Password
           </label>
